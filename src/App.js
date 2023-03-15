6c8e47bbe0b55469bacea356a7f4683da89a220c
Kushal.Vv <Kushal.Vv@Solere.com>
2023-03-15 13:56:49 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const VinDecoder = () => {
+  const [vin, setVin] = useState("");
+  const [year, setYear] = useState("");
+  const [make, setMake] = useState("");
+  const [model, setModel] = useState("");
+  const [fuelInjectionType, setFuelInjectionType] = useState("");
+  const [manufacturer, setManufacturer] = useState("");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (vin.length === 17) {
+      axios
+        .get(`https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/${vin}?format=json`)
+        .then((response) => {
+          const vehicle = response.data.Results[0];
+          setYear(vehicle.ModelYear);
+          setMake(vehicle.Make);
+          setModel(vehicle.Model);
+          setFuelInjectionType(vehicle.FuelInjectionType);
+          setManufacturer(vehicle.Manufacturer)
+
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      alert("Please enter a valid VIN");
+    }
+  }; 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>VIN Decoder</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="vin">Enter VIN:</label>
+        <input
+          type="text"
+          id="vin"
+          name="vin"
+          value={vin}
+          onChange={(e) => setVin(e.target.value)}
+        />
+        <button type="submit">Decode</button>
+      </form>
+      {year && make && model&& manufacturer && fuelInjectionType && (
+        <div>
+           <h2>Vehicle Information</h2>
+          <table>
+            <tr>
+              <td>VIN:</td>
+              <td>{vin}</td>
+            </tr>
+            <tr>
+              <td>Model:</td>
+              <td>{model}</td>
+            </tr>
+            <tr>
+              <td>Year:</td>
+              <td>{year}</td>
+            </tr>
+            <tr>
+              <td>Make:</td>
+              <td>{make}</td>
+            </tr>
+            <tr>
+              <td>Manufacturer:</td>
+              <td>{manufacturer}</td>
+            </tr>
+            <tr>
+              <td>FuelInjectionType:</td>
+              <td>{fuelInjectionType}</td>
+            </tr>
+           
+          </table>
+        </div>
+      )}
     </div>
+    </>
   );
-}
+};
 
-export default App;
+export default VinDecoder;
